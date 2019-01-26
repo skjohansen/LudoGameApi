db00be92795997bd52239c5140fbd5ca9a257c5a
Stephan Johansen <stephan@distancify.com>
2019-01-26 21:49:38 +0100

    Added controllers

---
+++ LudoWebApi/Controllers/LudoController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LudoWebApi.Controllers
+{
+    [Route("api/ludo")]
+    [ApiController]
+    public class LudoController : ControllerBase
+    {
+        
+        // GET: api/Ludo
+        /// <summary>
+        /// Lista av fia spel 
+        /// </summary>
+        /// <returns>Lista av fia spel </returns>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+        // POST: api/Ludo
+        /// <summary>
+        /// Skåpa ett nytt spel
+        /// </summary>
+        /// <param name="value"></param>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+
+        // GET: api/Ludo/5
+        /// <summary>
+        /// Detaljeret information om spelet, som vart alla pjäser finns
+        /// </summary>
+        /// <param name="gameId"></param>
+        /// <returns>Detaljeret information om spelet, som vart alla pjäser finns</returns>
+        [HttpGet("{gameId}")]
+        public string GetGame(int gameId)
+        {
+            return "value";
+        }
+
+        // PUT: api/Ludo/5
+        /// <summary>
+        /// Ändra placering på en pjäs
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        [HttpPut("{gameId}")]
+        public void PutGame(int gameId, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/Ludo/5
+        /// <summary>
+        /// Ta bort ett spel
+        /// </summary>
+        /// <param name="gameId"></param>
+        [HttpDelete("{gameId}")]
+        public void DeleteGame(int gameId)
+        {
+        }
+    }
+}
---
+++ LudoWebApi/Controllers/LudoPlayersController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LudoWebApi.Controllers
+{
+    [Route("api/ludo")]
+    [ApiController]
+    public class LudoPlayersController : ControllerBase
+    {
+
+        // GET: api/ludo/3/players
+        /// <summary>
+        /// List med alla spelera i spelet
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("{gameId}/players")]
+        public IEnumerable<string> Get(int gameId)
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+        // POST: api/ludo/3/players
+        /// <summary>
+        /// Lägg till en ny spelere till spelet
+        /// </summary>
+        /// <param name="value"></param>
+        [HttpPost("{gameId}/players")]
+        public void Post(int gameId, [FromBody] string value)
+        {
+        }
+
+
+        // GET: api/ludo/3/players/4
+        /// <summary>
+        /// Detajeret information om speleren
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{gameId}/players/{playerId}")]
+        public string Get(int gameId, int playerId)
+        {
+            return "value";
+        }
+
+        // PUT: api/ludo/3/players/4
+        /// <summary>
+        /// Ändra namn eller färg på speleren
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        [HttpPut("{gameId}/players/{playerId}")]
+        public void Put(int gameId, int playerId, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ludo/3/players/4
+        /// <summary>
+        /// Ta bort speleren
+        /// </summary>
+        /// <param name="id"></param>
+        [HttpDelete("{gameId}/players/{playerId}")]
+        public void Delete(int gameId, int playerId)
+        {
+        }
+    }
+}
