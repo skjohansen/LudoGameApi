f9d02a69e3d4dee6c68012924a4d827d4300d207
Stephan Johansen <stephan@distancify.com>
2019-01-27 10:56:03 +0100

    Added a winner controller

---
+++ LudoWebApi/Controllers/LudoWinnerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LudoWebApi.Controllers
+{
+    [Route("api/ludo")]
+    [ApiController]
+    public class LudoWinnerController : ControllerBase
+    {
+        private ILudoGameContainer ludoGames;
+
+        public LudoWinnerController(ILudoGameContainer lgc)
+        {
+            ludoGames = lgc;
+        }
+
+        // GET: api/ludo/3/winner
+        /// <summary>
+        /// Name of the winner (If the game is over)
+        /// </summary>
+        /// <param name="gameId"></param>
+        /// <returns></returns>
+        [HttpGet("{gameId}/winner")]
+        public string Get(int gameId)
+        {
+            return ludoGames[gameId].GetWinner().Name;
+        }
+    }
+}
